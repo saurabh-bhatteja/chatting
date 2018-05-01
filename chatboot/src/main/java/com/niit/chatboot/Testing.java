eb619f59282827360f39759703f4399d9abe188e
SAURABH BHATTEJA <SAURABH BHATTEJA@DESKTOP-GD0CRB2>
2018-05-01 08:05:17 +0530

    First Commit

---
+++ chatboot/src/main/java/com/niit/chatboot/Testing.java
@@ -0,0 +1,9 @@
+package com.niit.chatboot;
+
+public class Testing {
+	public static void main(String args[])
+	{
+	String one = ChatBot.chatbotmsgsnd("what are you doing");	
+	System.out.println("where are you from  "+" "+one);	
+	}
+}
